ac7a7ec32caca73edc92a3b36740b4b2f12171d4
Nate Duncan <60360613+ntduncan@users.noreply.github.com>
2024-05-21 22:16:13 -0600

    added parser error handling for parsing errors

--- parser/parser.go
+++ parser/parser.go
@@ -1,20 +1,25 @@
 package parser
 
 import (
+	"fmt"
 	"ntduncan.com/go-interpreter/ast"
 	"ntduncan.com/go-interpreter/lexer"
 	"ntduncan.com/go-interpreter/token"
 )
 
 type Parser struct {
-	l *lexer.Lexer
+	l      *lexer.Lexer
+	errors []string
 
 	curToken  token.Token
 	peekToken token.Token
 }
 
 func New(l *lexer.Lexer) *Parser {
-	p := &Parser{l: l}
+	p := &Parser{
+		l:      l,
+		errors: []string{},
+	}
 
 	p.nextToken()
 	p.nextToken()
@@ -31,7 +36,7 @@ func (p *Parser) ParseProgram() *ast.Program {
 	program := &ast.Program{}
 	program.Statements = []ast.Statement{}
 
-	for p.curToken.Type != token.EOF {
+	for !p.curTokenIs(token.EOF) {
 		stmt := p.parseStatement()
 		if stmt != nil {
 			program.Statements = append(program.Statements, stmt)
@@ -51,8 +56,18 @@ func (p *Parser) parseStatement() ast.Statement {
 	}
 }
 
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+func (p *Parser) peekError(t token.TokenType) {
+
+	msg := fmt.Sprintf("expected next toke to be %s, got %s instead", t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
+}
+
 func (p *Parser) parseLetStatement() *ast.LetStatement {
-	stmt := &ast.LetStatement{}
+	stmt := &ast.LetStatement{Token: p.curToken}
 
 	if !p.expectPeek(token.IDENT) {
 		return nil
@@ -83,6 +98,8 @@ func (p *Parser) expectPeek(t token.TokenType) bool {
 	if p.peekTokenIs(t) {
 		p.nextToken()
 		return true
+	} else {
+		p.peekError(t)
+		return false
 	}
-	return false
 }
--- parser/parser_test.go
+++ parser/parser_test.go
@@ -16,6 +16,7 @@ func TestLetStatements(t *testing.T) {
 	p := New(l)
 
 	program := p.ParseProgram()
+	checkParserErrors(t, p)
 
 	if program == nil {
 		t.Fatalf("ParseProgram() returned nil")
@@ -40,8 +41,22 @@ func TestLetStatements(t *testing.T) {
 	}
 }
 
+func checkParserErrors(t *testing.T, p *Parser) {
+	errors := p.Errors()
+
+	if len(errors) == 0 {
+		return
+	}
+
+	t.Errorf("parser has %d errors", len(errors))
+	for _, msg := range errors {
+		t.Errorf("parser error: %q", msg)
+	}
+	t.FailNow()
+}
+
 func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
-	t.Logf("s.TokenLiteral = %q", s)
+	t.Logf("s.TokenLiteral = %q", s.TokenLiteral())
 
 	if s.TokenLiteral() != "let" {
 		t.Errorf("s.TokenLiteral not 'let'. got=%q", s.TokenLiteral())
