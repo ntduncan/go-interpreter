f3e27d0a4f49b1e72067a17d45dd114dbd2c30e8
Nate Duncan <60360613+ntduncan@users.noreply.github.com>
2024-07-19 17:19:04 -0600

    function literal parsing

--- ast/ast.go
+++ ast/ast.go
@@ -3,6 +3,7 @@ package ast
 import (
 	"bytes"
 	"ntduncan.com/go-interpreter/token"
+	"strings"
 )
 
 type Node interface {
@@ -201,7 +202,7 @@ func (ie *IfExpression) String() string {
 	return out.String()
 }
 
-// BLOCK STATEMENTS
+// BLOCK q
 type BlockStatement struct {
 	Token      token.Token //LBRACE to show opening of statement
 	Statements []Statement
@@ -215,5 +216,28 @@ func (bs *BlockStatement) String() string {
 		out.WriteString(s.String())
 	}
 
+	return out.String()
+}
+
+// FUNCTION LITERALS
+type FunctionLiteral struct {
+	Token      token.Token
+	Parameters []*Identifier
+	Body       *BlockStatement
+}
+
+func (fl *FunctionLiteral) expressionNode()      {}
+func (fl *FunctionLiteral) TokenLiteral() string { return fl.Token.Literal }
+func (fl *FunctionLiteral) String() string {
+	var out bytes.Buffer
+	params := []string{}
+	for _, p := range fl.Parameters {
+		params = append(params, p.String())
+	}
+	out.WriteString(fl.TokenLiteral())
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") ")
+	out.WriteString(fl.Body.String())
 	return out.String()
 }
